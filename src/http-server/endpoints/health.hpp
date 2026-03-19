cf1a7a05c1fbc680b1b343ebc88787915f18c480
Mahad Siddiqui <mahadsiddiqui07@outlook.com>
2026-03-19 14:20:30 +0000

    sophisticated health

--- src/http-server/endpoints/health.hpp
+++ src/http-server/endpoints/health.hpp
@@ -1,6 +1,30 @@
 #pragma once
 
 #include "httplib.h"
+#include "json.hpp"
+#include <fstream>
+
+using nlohmann::json;
 
 // Simple health check to see if the http-server is up
-inline void health(const httplib::Request& _, httplib::Response& res) { res.status = 200; res.set_content("Hello World!", "text/plain"); }
+inline void health(const httplib::Request& _, httplib::Response& res) {
+	json status;
+	std::ifstream file("state/status.json");
+
+	if (!file.is_open()) {
+		std::cerr << "Failed to open file 'state/status.json'.\n";
+		res.status = 500;
+		res.set_content("Failed to open file 'state/status.json'.\n", "text/plain");
+		return;
+	}
+
+	try { file >> status; }
+	catch (std::exception) {
+		res.status = 500;
+		res.set_content("Internal JSON was malformed!", "text/plain");
+		return;
+	}
+
+	res.status = 200;
+	res.set_content(status.dump(4), "application/json");
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <vector>
+#include <fstream>
+#include "json.hpp"
+
+using nlohmann::json;
 
 std::vector<pid_t> children;
+json status_json;
 
 void kill_children() {
 	for (auto pid : children) {
@@ -19,15 +25,19 @@ void sig_handler(int _) {
 	_exit(1);
 }
 
-pid_t spawn(const char *path) {
+pid_t spawn(const char *path, const char* nice_name) {
+	status_json[nice_name] = "starting...";
     pid_t pid = fork();
     if (pid == 0) {
+		status_json[nice_name] = "running";
         execl(path, path, (char *)NULL);
         perror("execl failed");
         _exit(1);
+		status_json[nice_name] = "execl failed";
     }
     if (pid < 0) {
         perror("fork failed");
+		status_json[nice_name] = "fork failed";
     }
 
 	children.push_back(pid);
@@ -40,15 +50,16 @@ int main() {
     signal(SIGTERM, sig_handler);
 	atexit(kill_children);
 
-    pid_t http_pid = spawn("./http-server/http-server");
-    pid_t udp_pid  = spawn("./udp-server/udp-server");
+	std::system("mkdir -p state && touch state/status.json");
+    pid_t http_pid = spawn("./http-server/http-server", "http");
+    pid_t udp_pid  = spawn("./udp-server/udp-server", "http");
 
     while (1) {
         if (http_pid <= 0)
-            http_pid = spawn("./http-server/http-server");
+            http_pid = spawn("./http-server/http-server", "http");
 
         if (udp_pid <= 0)
-            udp_pid = spawn("./udp-server/udp-server");
+            udp_pid = spawn("./udp-server/udp-server", "http");
 
         int status;
         pid_t r;
@@ -66,6 +77,17 @@ int main() {
 		}
 
 		children = new_children;
+
+		// Write status to status.json for the http server
+		std::ofstream file("state/status.json");
+		if (!file.is_open()) {
+			std::cerr << "Failed to open file 'state/status.json'.\n";
+			return 1;
+		}
+
+		file << status_json.dump(4);
+
+		file.close();
 
         sleep(5);
     }
